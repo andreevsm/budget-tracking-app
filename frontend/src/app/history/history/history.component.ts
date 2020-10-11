bfa3b0a00c525b317524e6efd35f40b4951bafa2
andreevsm <sergey-andreev-97-97@mail.ru>
2020-10-11 11:09:27 +0300

    Add aliases

--- frontend/src/app/history/history/history.component.ts
+++ frontend/src/app/history/history/history.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { IExpense, ExpansesActions } from 'src/app/core/store';
+
+import { ExpansesState } from '../../core/store/expanses/expanses.state';
 
 @Component({
   selector: 'bg-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HistoryComponent implements OnInit {
+  @Select(ExpansesState.expanses) public expanses$: Observable<IExpense[]>;
 
-  constructor() { }
+  constructor(private store: Store) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.store.dispatch(new ExpansesActions.LoadAll());
   }
-
 }
